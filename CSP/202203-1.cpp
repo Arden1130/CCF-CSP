4d4d54766d0e909c2f830dbe82d155fb238aa0bd
Arden <1328635912@qq.com>
2023-03-06 12:13:51 +0800

    202203-1

--- CSP/202203-1.cpp
+++ CSP/202203-1.cpp
@@ -6,7 +6,7 @@ int main() {
 	cin >> n >> k;
 	//int a[n]={0}或者int a[n]={}不都可以将元素初始化为0吗？可是这样写只有70分我真的不懂
 	int* a = new int[n];
-	memset(a, 0, sizeof(int) * n);
+	memset(a, 0, sizeof(int) * n);//memset函数头文件为cstring
 	//当然for循环初始化也可以
 	for (int i = 1; i <= k; i++) {
 		cin >> x >> y;
